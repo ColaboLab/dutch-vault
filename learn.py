56df2c051cb180c1eae3518267c7184a626159d4
emilmub <emilmubarqui@gmail.com>
2025-02-17 13:11:32 +0100

    Store verbs

--- learn.py
+++ learn.py
@@ -1,5 +1,5 @@
 from db import session
-from db.models import Noun, NounArticle
+from db.models import Noun, NounArticle, Verb, Conjugation
 import argparse
 
 def store_new_noun(noun, article):
@@ -19,8 +19,17 @@ def het_of_de(noun):
     if not existing_noun:
         print("We have not registered that noun yet.")
     else:
-        article = session.query(NounArticle.article).filter(NounArticle.noun_id == existing_noun.id).all()
-        print(article)
+        results = session.query(NounArticle.article).filter(NounArticle.noun_id == existing_noun.id).all()
+        print([result.article for result in results])
+
+def store_new_verb(verb):
+    existing_verb = session.query(Verb).filter(Verb.infinitive == verb.lower()).first()
+    if not existing_verb:
+        existing_verb = Verb(infinitive = verb.lower())
+        session.add(existing_verb)
+        session.commit()
+    else:
+        print("Verb already exists, use FUNCTION to consult conjugation")
 
 def main():
     parser = argparse.ArgumentParser()
@@ -35,6 +44,10 @@ def main():
     add_het_of_de_parser = subparsers.add_parser("het-of-de", help="Retrieves article for noun")
     add_het_of_de_parser.add_argument("noun", type=str, help="The noun word")
     add_het_of_de_parser.set_defaults(func=lambda args: het_of_de(args.noun))
+
+    add_verb_parser = subparsers.add_parser("add-verb", help = "Add a new verb")
+    add_verb_parser.add_argument("verb", type=str, help="The verb in infinitive")
+    add_verb_parser.set_defaults(func=lambda args: store_new_verb(args.verb))
 
     args = parser.parse_args()
     args.func(args)
